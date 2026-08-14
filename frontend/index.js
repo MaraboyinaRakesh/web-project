7be05ff33c7ed4a74321b6563885dbc06dfccddf
MaraboyinaRakesh <maraboyinarakesh666@gmail.com>
2026-08-14 10:09:43 +0530

    frontend,backend update

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,38 @@
-alert("hello from javasrcipt")
+// alert("hello from javasrcipt")
+const users = [
+ {
+   "name" : "Jonn Doe",
+   "gender" : "Male",
+   "image" : "john.png"
+ },
+   {
+   "name" : "Jane Doe",
+   "gender" : "Female",
+   "image" : "jane.png"
+ }
+]
+
+
+var curIndex = 0;
+
+
+function toggle(){
+ if(curIndex == 0)
+   curIndex = 1;
+ else
+   curIndex = 0;
+
+
+ document.getElementById("card-image").src = users[curIndex].image
+ document.getElementById("card-name").innerText =users[curIndex].name
+ document.getElementById("card-gender").innerText=users[curIndex].gender
+}
+
+
+function toggle(){
+    if(curIndex==0){
+        curIndex=1;
+    }else{
+        
+    }
+}
